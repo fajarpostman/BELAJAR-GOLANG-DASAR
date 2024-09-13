d141ca181fb577dc3d0dd22a43a0fd40645ba2a7
Fajar Dwi Rianto <fajar@klikkami.co.id>
2024-09-13 17:11:46 +0700

    update new go

---
+++ section-1/slice.go
@@ -0,0 +1,11 @@
+// Tipe Data Slice
+// - Tipe data slice adalah potongan dari data array
+// - Slice mirip dengan Array, yang membedakan adalah ukuran Slice bisa berubah
+// - Slice da Array selalu terkoneksi, dimana Slice adalah data yang mengakses sebagaian atau seluruh data di Array
+
+// Detail Tipe Data Slice
+// - Tipe Data Slice memiliki 3 data, yaitu pointer, length dan capacity
+// - Pointer adalah penunjuk data pertama di array para slice
+// - Lengh adalah panjang dari slice, dan
+// - Capacity aalah kapasitas dari slice, dimana length tidak boleh lebih dari capacity
+
